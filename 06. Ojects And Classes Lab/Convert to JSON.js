3d86fa8bd6cc57749e7beec276ef056669edf3c1
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2020-06-23 07:31:54 +0300

    Delete Convert to JSON.js

--- 06. Ojects And Classes Lab/Convert to JSON.js
+++
@@ -1,6 +0,0 @@
-function convertToJson(name, lastName, hairColor) {
-  let obj = { name: name, lastName: lastName, hairColor: hairColor };
-  let str = JSON.stringify(obj);
-  console.log(str);
-}
-convertToJson("George", "Jones", "Brown");
