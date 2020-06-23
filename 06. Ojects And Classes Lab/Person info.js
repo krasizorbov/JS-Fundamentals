6fc11ae5cd0b8640f24fc8064140d2695ce4328b
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2020-06-23 07:32:06 +0300

    Delete Person info.js

--- 06. Ojects And Classes Lab/Person info.js
+++
@@ -1,8 +0,0 @@
-function personInfo(firstName, lastName, age) {
-  let person = { firstName: firstName, lastName: lastName, age: age };
-  let entries = Object.entries(person);
-  for (const [key, value] of entries) {
-    console.log(`${key}: ${value}`);
-  }
-}
-personInfo("Peter", "Pan", "20");
