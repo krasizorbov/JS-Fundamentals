fe46ccd26a761437a6700438bac6fa0234b0747c
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2020-06-23 07:32:00 +0300

    Delete Convert to object.js

--- 06. Ojects And Classes Lab/Convert to object.js
+++
@@ -1,8 +0,0 @@
-function convertToObject(input) {
-  let obj = JSON.parse(input);
-  let entries = Object.entries(obj);
-  for (const [key, value] of entries) {
-    console.log(`${key}: ${value}`);
-  }
-}
-convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
