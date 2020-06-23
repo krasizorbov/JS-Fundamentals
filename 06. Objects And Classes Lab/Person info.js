f6007964a71a7fc72106e7da0ad013e944b4dd35
Krasimir Zorbov <49094272+krasizorbov@users.noreply.github.com>
2020-06-23 07:32:39 +0300

    Add files via upload

---
+++ 06. Objects And Classes Lab/Person info.js
@@ -0,0 +1,8 @@
+function personInfo(firstName, lastName, age) {
+  let person = { firstName: firstName, lastName: lastName, age: age };
+  let entries = Object.entries(person);
+  for (const [key, value] of entries) {
+    console.log(`${key}: ${value}`);
+  }
+}
+personInfo("Peter", "Pan", "20");
